3e7f9547ffadd8cd5c1aeaebe87f49773de4dc16
Martin Müller <martin.mueller@4voice.de>
2024-02-21 15:52:25 +0100

    Projektdateien hinzufügen.

---
+++ IronPythonTest/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace IronPythonTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userControl11 = new WindowsFormsControlLibrary1.UserControl1();
+            this.SuspendLayout();
+            // 
+            // userControl11
+            // 
+            this.userControl11.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.userControl11.Location = new System.Drawing.Point(108, 165);
+            this.userControl11.Name = "userControl11";
+            this.userControl11.Size = new System.Drawing.Size(317, 122);
+            this.userControl11.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.userControl11);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private WindowsFormsControlLibrary1.UserControl1 userControl11;
+    }
+}
+
---
+++ WindowsFormsControlLibrary1/UserControl1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsControlLibrary1
+{
+    public partial class UserControl1: UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        public void ShowPythonGreeting()
+        {
+            var eng = IronPython.Hosting.Python.CreateEngine();
+
+            var scope = eng.CreateScope();
+
+            eng.Execute(@"
+def greetings(name):
+    return 'Hello ' + name.title() + '!'
+", scope);
+            dynamic greetings = scope.GetVariable("greetings");
+
+            MessageBox.Show(greetings("world"), "Greeting from IronPython");
+        }
+
+        private void UserControl1_MouseClick(object sender, MouseEventArgs e)
+        {
+            ShowPythonGreeting();
+        }
+    }
+}
